f9398d9fbe9afff0f67d72495a18aeb69546ba2c
Jerome Raymond <ronnie173@gmail.com>
2017-09-10 16:12:25 -0500

    Added nav and footer

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>About</h1>
+    <p> Powers, lovers, and alchemistic seekers will always protect them.Avast, yer not marking me without a power!Congregabo sed mire ducunt ad ferox assimilatio.Exsuls sunt liberis de fatalis pulchritudine.
+        Tribbles die from minerals like unrelated dosis. Assimilatios tolerare in tubinga!Sectams persuadere, tanquam domesticus vortex.Eheu, repressor!Rectors sunt finiss de clemens gemna.</p>
+
+@endsection
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,8 @@
+
+@section('sidebar')
+    <div class="well">
+        <h3>Sidebar</h3>
+        This is a side bar
+
+    </div>
+@show
